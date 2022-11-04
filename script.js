bb0d21be3b68effa379548b14b6c7fea4804f2a4
Andre Candido <andre.eng1313@gmail.com>
2022-11-03 22:59:19 -0300

    Atualizando

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ function renderizar () {
     for (let i=0; i<todosQuizzes.length; i++) {
         let objeto = todosQuizzes[i];
   
-        rend.innerHTML +=  `<div class="teste" onclick = "teste2 (${objeto.id})"> <img class="img-quizz" src="${objeto.image}"/>  <p> ${objeto.title} </p> </div> </div>`;
+        rend.innerHTML +=  `<div class="gameQuizzes" onclick = "segundaTela (${objeto.id})"> <img class="img-quizz" src="${objeto.image}"/>  <p> ${objeto.title} </p> </div> </div>`;
             
     };
 };
 
 
-    function teste2(id) {
+    function segundaTela(id) {
         console.log(id);
 
     }
@@ -52,51 +52,6 @@ function renderizar () {
 
 
 
-
-
-
-
-
-
-
-
-
-    // const nQuizzes = quizzes.length;
-    // let isYourQuizz = false;
-    
-    //percorrer lista de quizzes e imprimir a preview de cada um
-//     for(let i = 0; i < quizzes; i++){
-        
-//         const img = quizzes[i].image;
-//         const title = quizzes[i].title;
-//         const id = quizzes[i].id;
-        
-//         // if(your_ids_array !== null){
-//         //     for(let j = 0; j < your_ids_array.length; j++){
-//         //         if(quizzes[i].id === your_ids_array[j]){
-//         //             renderQuizz(todosQuizzes, img, title, id);
-//         //             isYourQuizz = true;
-//         //         }
-//         //     }
-//         // }
-//         // if(!isYourQuizz){
-//         //     renderQuizz(todosQuizzes, img, title, id);
-//         // }
-//         // isYourQuizz = false;
-        
-//     }
-// }
-
-// function renderizarQuizz(span, img, title, id) {
-//     span.innerHTML +=
-//     `
-//     <div class="quizz-preview" id="${id}" onclick="takeThisQuizz(this)" data-identifier="quizz-card">
-//         <img src="${img}" alt="quizz preview">
-//         <p>${title}</p>
-//     </div>
-//     `;
-    
-    //onclick directs user to that quizz
 function terceiraTela(){
     const paginaAtual = document.querySelector(".atual");
     const paginaDesejada = document.querySelector(".terceiraTela");
