6dff02f551c5644ed85b358012510209d76229dc
MatheusCerutti <matheuscerutti96@gmail.com>
2022-11-03 23:12:57 -0300

    aceitando o pull

--- script.js
+++ script.js
@@ -48,58 +48,6 @@ function teste2(id) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const nQuizzes = quizzes.length;
-// let isYourQuizz = false;
-
-//percorrer lista de quizzes e imprimir a preview de cada um
-//     for(let i = 0; i < quizzes; i++){
-
-//         const img = quizzes[i].image;
-//         const title = quizzes[i].title;
-//         const id = quizzes[i].id;
-
-//         // if(your_ids_array !== null){
-//         //     for(let j = 0; j < your_ids_array.length; j++){
-//         //         if(quizzes[i].id === your_ids_array[j]){
-//         //             renderQuizz(todosQuizzes, img, title, id);
-//         //             isYourQuizz = true;
-//         //         }
-//         //     }
-//         // }
-//         // if(!isYourQuizz){
-//         //     renderQuizz(todosQuizzes, img, title, id);
-//         // }
-//         // isYourQuizz = false;
-
-//     }
-// }
-
-// function renderizarQuizz(span, img, title, id) {
-//     span.innerHTML +=
-//     `
-//     <div class="quizz-preview" id="${id}" onclick="takeThisQuizz(this)" data-identifier="quizz-card">
-//         <img src="${img}" alt="quizz preview">
-//         <p>${title}</p>
-//     </div>
-//     `;
-
-//onclick directs user to that quizz
-
-
-
 function terceiraTela() {
     const paginaAtual = document.querySelector(".atual");
     const paginaDesejada = document.querySelector(".terceiraTela");
